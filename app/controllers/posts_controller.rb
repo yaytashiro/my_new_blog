c7ee0f1be6f240e7b27be45559740a78f6223e2d
田代やよい <o.841.312@gmail.com>
2016-02-21 17:37:09 +0900

    new と createを実装して、ついでにeditの画面も編集

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,7 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def new
@@ -11,14 +12,27 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to @post
   end
 
   def edit
+    @posts= Post.find(params[:id])
   end
 
   def update
   end
 
   def delete
+  end
+
+  private
+  def post_params
+      params.require(:post).permit(
+        :title,
+        :body,
+        :category
+      )
   end
 end
