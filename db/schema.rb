ba8b75e824765debc47e48301d963a7570f59a0a
田代やよい <o.841.312@gmail.com>
2016-02-21 16:34:39 +0900

    category modelを追加 rails g model category title:string =>rake db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160221072222) do
+ActiveRecord::Schema.define(version: 20160221073056) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name",        null: false
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20160221072222) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "posts", force: :cascade do |t|
